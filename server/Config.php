561d99e9f7b8eea0b9b2efec34b10095cc247c92
Piotr Janczura <piotr@janczura.pl>
2015-11-30 12:17:05 +0000

    Config.php edited online with Bitbucket

--- server/Config.php
+++ server/Config.php
@@ -4,9 +4,6 @@ class Config {
 	const DB_TYPE    = 'mysql';
 	const DB_CHARSET = 'utf8';
 	const DB_PORT    = 3306;
-	const DB_NAME    = 'radmin_16';
-	const DB_USER    = 'radmin_16';
-	const DB_PASS    = 'PJPL_radmin_16';
 	const DB_NAME    = '*';
 	const DB_USER    = '*';
 	const DB_PASS    = '*';
