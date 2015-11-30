f48b4e0b933d10163552fde58dc85459df3a382e
Piotr Janczura <piotr@janczura.pl>
2015-11-30 12:10:08 +0000

    Config.php edited online with Bitbucket

--- server/Config.php
+++ server/Config.php
@@ -4,7 +4,7 @@ class Config {
 	const DB_TYPE    = 'mysql';
 	const DB_CHARSET = 'utf8';
 	const DB_PORT    = 3306;
-	const DB_NAME    = 'dealercrm';
-	const DB_USER    = 'dealercrm';
-	const DB_PASS    = 'dealercrm';
+	const DB_NAME    = '*';
+	const DB_USER    = '*';
+	const DB_PASS    = '*';
 }
