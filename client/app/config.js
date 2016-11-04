d5a6679a7da9ec92481043cc482637bf5537d8a3
Piotr Janczura <piotr@janczura.pl>
2016-11-04 11:07:10 +0100

    angular 1.5

--- client/app/config.js
+++ client/app/config.js
@@ -16,8 +16,8 @@ var CF = {
 };
 
 CF.ex = {		// Wyjątki zgłaszane przez server
-	cod : {}, // Liczbowy kod wyjątku
-	mnm : {}, // Krótka nazwa wyjątku
+	cod : {},	// Liczbowy kod wyjątku
+	mnm : {},	// Krótka nazwa wyjątku
 	msg : {}	// Wiadomość opisowa wyjątku
 };
 CF.ex.cod.CHAT = 1;
@@ -39,14 +39,14 @@ CF.ex.cod.USER_CREATE_EXISTS = 6;
 CF.ex.mnm.USER_CREATE_EXISTS = 'EX_USER_CREATE_EXISTS';
 CF.ex.msg.USER_CREATE_EXISTS = 'Użytkownik o podanej nazwie już ustnieje.';
 CF.ex.cod.USER_CREATE_NICK = 7;
-CF.ex.cod.USER_CREATE_NICK = 'EX_USER_CREATE_NICK';
-CF.ex.cod.USER_CREATE_NICK = 'Niepoprawny nick';
+CF.ex.mnm.USER_CREATE_NICK = 'EX_USER_CREATE_NICK';
+CF.ex.msg.USER_CREATE_NICK = 'Niepoprawny nick';
 CF.ex.cod.MESSAGE_CREATE = 8;
 CF.ex.mnm.MESSAGE_CREATE = 'EX_MESSAGE_CREATE';
 CF.ex.msg.MESSAGE_CREATE = 'Prblem z wysłaniem wiadomości.';
 CF.ex.cod.MESSAGE_SELECT = 9;
 CF.ex.mnm.MESSAGE_SELECT = 'EX_MESSAGE_SELECT';
-CF.ex.cod.MESSAGE_SELECT = '';
+CF.ex.msg.MESSAGE_SELECT = '';
 CF.ex.cod.SESSION_TROUBLE = 10;
 CF.ex.mnm.SESSION_TROUBLE = 'EX_SESSION_TROUBLE';
-CF.ex.cod.SESSION_TROUBLE = '';
+CF.ex.msg.SESSION_TROUBLE = '';
