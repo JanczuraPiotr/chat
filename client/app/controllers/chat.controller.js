c0d0d54b651b88c505958f0d587a5b17f60660a2
Piotr Janczura <piotr@janczura.pl>
2016-11-07 18:34:31 +0100

    angular 1.5

--- client/app/controllers/chat.controller.js
+++ client/app/controllers/chat.controller.js
@@ -3,15 +3,14 @@
 angular.module('app').controller('ChatController',[
 	'$scope',
 	'$http',
-	'$location',
+	'$timeout',
 	'$interval',
 	'AuthenticationService',
 
-	function($scope, $http, $location, $interval, AuthenticationService){ console.log('ChatController');
+	function($scope, $http, $timeout, $interval, AuthenticationService){ console.log('ChatController');
 		var def = this;
 		def.run = null;
 		def.lastTimestamp = '';
-
 		$scope.newPost;
 		$scope.posts = {};
 		$scope.posts.store = {};
@@ -38,6 +37,8 @@ angular.module('app').controller('ChatController',[
 					}
 				}
 			},function(error){console.log('ChatController.apiReadAll().error');
+
+			}).finally(function(){console.log('finally');
 
 			});
 		};
@@ -122,113 +123,9 @@ angular.module('app').controller('ChatController',[
 			AuthenticationService.logout();
 		};
 
-		var __def__ = this;
-		__def__.myFormId   = config.dom.formChat.me.id;
-		__def__.chatEditId = config.dom.formChat.chatEdit.me.id;
-		__def__.chatAllId  = config.dom.formChat.chatAll.me.id;
-		__def__.btnSendId  = config.dom.formChat.btnSend.me.id;
-		__def__.btnExitId  = config.dom.formChat.btnExit.me.id;
-
-
-		__def__.dom = {};
-		__def__.dom.clTest = true;
-		__def__.dom.addPost = function(nick, post, timestamp){
-			$(__def__.chatAllId).append(__def__.dom.postTemplate(nick,post,timestamp));
-			$(__def__.chatAllId).scrollTop($(__def__.chatAllId)[0].scrollHeight);
-		};
-
-//		__def__.ajaxSend = function(post){
-//			// @todo sporadycznie dodany post wyświetlany jest przez ajax dodający i ajas czytajacy nowe posty
-//			$.ajax({
-//				url : config.url.server.chatAdd,
-//				data : {
-//					timestamp : def.lastTimestamp, // zaraz po zapisie nowego posta pobiera posty powstałe po tym czasie
-//					post : post
-//				},
-//				success : function(responseText, status, xhr){
-//					var resp = JSON.parse(responseText);
-//					switch (resp.ret){
-//						case 'OK':
-//							for ( post in resp.data){
-//								if( def.lastTimestamp < resp.data[post].timestamp ){
-//									def.lastTimestamp = resp.data[post].timestamp;
-//								}
-//								__def__.dom.addPost(resp.data[post].nick, resp.data[post].post, resp.data[post].timestamp);
-//							}
-//							break
-//						default:
-//							if(resp.data.supplement){
-//								alert(resp.msg + '\n objaśnienie : \n' + resp.data.supplement);
-//							}else{
-//								alert(resp.msg);
-//							}
-//					}
-//				}
-//			});
-//		};
-//		__def__.ajaxCommand = function(command){
-//			// @todo o wykonanej komendzie należy poinformować wszystkich uruchomionych klientów
-//			$.ajax({
-//				url : config.url.server.chatCommand,
-//				data : {
-//					action : command
-//				},
-//				success : function(responseText, status, xhr){
-//					var resp = JSON.parse(responseText);
-//					switch (resp.ret){
-//						case 'OK':
-//							$(__def__.chatAllId).html('');
-//							break;
-//						default:
-//							if(resp.data.supplement){
-//								alert(resp.msg + '\n objaśnienie : \n' + resp.data.supplement);
-//							}else{
-//								alert(resp.msg);
-//							}
-//					}
-//				}
-//			});
-//		};
-
-		def.thread = def.apiReadLast;
-
-		__def__.on = {};
-
-		__def__.on.clickBtnSend = function(){
-			var post = $(__def__.chatEditId, __def__.myFormId).val().trim();
-
-			switch( post) {
-				case '#clear#' :
-					__def__.ajaxCommand('clear');
-					break;
-				__def__ault:
-					__def__.ajaxSend(post);
-			}
-
-			$(__def__.btnSendId, __def__.myFormId).prop('disabled', true);
-			$(__def__.chatEditId, __def__.myFormId).val('');
-		};
-		__def__.on.clickBtnExit = function(){
-			__def__.ajaxExit();
-		};
-		__def__.on.changeChatEdit = function(e){
-			var strVal = $(__def__.chatEditId, __def__.myFormId).val();
-			if(strVal.length > 0 ){
-				$(__def__.btnSendId, __def__.myFormId).prop('disabled', false);
-			}else{
-				$(__def__.btnSendId, __def__.myFormId).prop('disabled', true);
-			}
-			if(e.keyCode === 13){
-				$(__def__.btnSendId).click();
-			}
-		};
-
-
-		//------------------------------------------------------------------------------
-
-
 		def.init = function(){
 			def.apiReadAll();
+			def.thread = def.apiReadLast;
 			def.run = $interval(def.thread, CF.thread.TIME_INTERVAL);
 		};
 
--- client/app/directives/scroll-posts.js
+++ client/app/directives/scroll-posts.js
@@ -1,7 +1,19 @@
-app.directive('scrollPosts', function () {
+app.directive('scrollPosts',function ($timeout, $http) {
+		var el;
+
     return {
         restrict: 'A',
         link: function (scope, element, attrs) {console.log('directive.scrollPosts');
+
+					// Nie pracuje w Firefoxie !
+					var wait = function(){
+						if($http.pendingRequests.length > 0) {
+							$(element).scrollTop($(element)[0].scrollHeight);
+						} else {
+							$timeout(wait,1);
+						}
+					};
+					wait();
 
 					scope.$watch('posts.store',function(){
 						$(element).scrollTop($(element)[0].scrollHeight);
